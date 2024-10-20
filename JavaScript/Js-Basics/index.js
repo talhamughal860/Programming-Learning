5603e3e37acb9202b3d92a723486650c7648419c
Talha Mughal <talhaaslam860@gmail.com>
2024-10-20 04:36:45 -0700

    type conversion+ data types+ basic Js+ operators

--- JavaScript/Js-Basics/index.js
+++ JavaScript/Js-Basics/index.js
@@ -1,9 +1,92 @@
 
 // ======================= JS BASICS ===================== //
 
-
-
 // --------------------- 20 OCT 2024 --------------------- //
 
+
+// categories there are two categories of data type premitive? and non-premitive? 
+// premitive: number, string, bloean, undefined, null.
+// non-premitive: object, array, function.
+// why premitive: one value and non-premitive: multible values?
+// interpreter and compiler scripting and programming language?
+ 
+
+// var x = 7;
+// const y = 5;
+// var z = x + y;
+// console.log(z);
+
+// let favdrink = "Roh afzah";
+// let price = 450;
+
+// let myfav = function() {
+//     console.log(favdrink, price);
+//     console.log(typeof favdrink,price);
+//     // can we find type of two variable by using comma ","
+
+// }
+
+// myfav();
+
+
+// let person = {
+//     name : "Talha Mughal",
+//     age : 28,
+//     email : "talha@blankslate.pk",
+// }
+
+// console.log(person.age);
+
+
+// arr = ["talha", 28, "info@blankslate.pk"];
+// console.log(arr[1]);
+
+// var n1 = 5;
+// var n2 = "7";
+
+// console.log(n1 == n2);
+
+// "Error arha hy ismy"
+// var age = 35;
+
+// if (age >= 18 && age <= 40){
+//     document.write("This is true");
+// }
+
+// there are 5 types of operators.
+// 1) Arthemetic Operators:
+//  + - / * ++ --
+
+// 2) Logical Operators:
+//  < >  !< !>
+
+// 3) Assignment Operators:
+//   = += -= 
+
+// 4) comparison Operators:
+//  == === != >= <= 
+
+// 5) Conditional Operators:
+//  True false
+
+// variables in js
+//  there are only 3 types of varaibles
+// 1) var 
+// example: 
+
+// const num1 = 20;
+//     num1 = "This is numbner";
+
+//     console.log(typeof num1);
+
+
+// function talha(){
+//     let wetech = 20;
+// }
+
+//  ++++++++++++++++++++++++ TYPE CONVERSION +++++++++++++++++++++++++++++
+// var talha = "20";
+// var aslam = Number(talha);
+// console.log(aslam);
 
 
